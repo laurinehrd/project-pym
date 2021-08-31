bd4a647ce61bfeafccdb09b7ded61f8665d0da0b
laurinehrd <laurine.hrd@hotmail.fr>
2021-08-31 14:10:35 +0000

    add namespace for UserRepository : pb authentification resolve

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
